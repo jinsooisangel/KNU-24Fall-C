5b8b2b8df086f0af286496b4b4e4d3f54ac97e10
jinsooisangel <bangjinseo90@gmail.com>
2024-09-12 16:32:10 +0900

    Create day02-1.c
    사칙연산 프로그램

---
+++ day02/day02/day02-1.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main() {
+
+	printf("원하는 기능을 입력하세요.\n");
+	printf("1. 더하기 2. 빼기 3. 곱하기 4. 나누기\n");
+	printf("기능: ");
+
+	int oper;
+	scanf_s("%d", &oper);
+
+	double num1, num2;
+	printf("숫자 1: ");
+	scanf_s(" % lf", &num1);
+
+	printf("숫자 2: ");
+	scanf_s("% lf", &num2);
+
+	double result;
+	if (oper == 1) {
+		result = num1 + num2;
+	}
+	else if (oper == 2) {
+		result = num1 - num2;
+	}
+	else if (oper == 3) {
+		result = num1 * num2;
+	}
+	else
+	{
+		result = num1 / num2;
+	}
+	printf("result= %lf", result);
+
+	return 0;
+}
